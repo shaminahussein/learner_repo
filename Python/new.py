abc01547667316a2b9333da3318c7dfce3be5541
shaminahussein <shamina_87@hotmail.com>
2024-11-01 09:00:35 +1000

    1

---
+++ Python/new.py
@@ -0,0 +1,14 @@
+def printToScreen():
+    print('This is what is part of this function')
+    age = 28
+    print(f'Age is {age}')
+
+printToScreen()
+
+def addNumbers(a,b):
+    return a+b
+
+print(addNumbers(2,3))
+
+sum = addNumbers(2,3)
+print(f'sum: {sum}')
